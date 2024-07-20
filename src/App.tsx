86fe4f6e440fc91095177cf92b3d2e940f38a47b
Manish rawat <manish.rawat@essencesoftwares.com>
2024-07-20 16:38:07 +0530

    local bootstrap added but conflicting classes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import './App.css'
+import LoginComponent from './pages/login';
 
 function App() {
   return (
-    <>
-      <h1 className='text-gray-200'>Hello</h1>
-    </>
+      <LoginComponent />
   );
 }
 
---
+++ src/pages/login/index.tsx
@@ -0,0 +1,11 @@
+const LoginComponent = () => {
+  return (
+    <>
+        <div className="container">
+            <h1 className="text-gray-300">Login page will be here</h1>
+        </div>
+    </>
+  )
+}
+
+export default LoginComponent
