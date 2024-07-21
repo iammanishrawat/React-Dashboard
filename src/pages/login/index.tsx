74819b5aecfb975a5664e154f58c4042532ce206
Manish rawat <manish.rawat@essencesoftwares.com>
2024-07-21 12:34:20 +0530

    shad cn added

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,8 +1,10 @@
+import { Button } from "@/components/ui/button"
 const LoginComponent = () => {
   return (
     <>
         <div className="container">
             <h1 className="text-gray-300">Login page will be here</h1>
+            <Button variant={"outline"}>Click me</Button>
         </div>
     </>
   )
