68aefee106541e9f17b750e80b160641b9ce0417
Manish rawat <manish.rawat@essencesoftwares.com>
2024-07-22 01:21:14 +0530

    toolkit and dashboard component added

---
+++ src/components/topbar/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TopbarComponent = () => {
+  return (
+    <div>TopbarComponent</div>
+  )
+}
+
+export default TopbarComponent
---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,14 @@
+import SidebarComponent from '@/components/sidebar'
+import TopbarComponent from '@/components/topbar'
+// import React from 'react'
+
+const DashboardComponent = () => {
+  return (
+    <>
+        <SidebarComponent/>
+        <TopbarComponent/>
+    </>
+  )
+}
+
+export default DashboardComponent
